3b38d40900ed5f9562f1d719d4b2362b9c2af07c
Daniil <93822098+Daniil8k@users.noreply.github.com>
2022-02-19 19:48:42 +0300

    init, cats example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,95 @@
 import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'grid-flatlist-react-native';
+import { useState, useEffect } from 'react';
+import { StatusBar, StyleSheet, View, Text, Image } from 'react-native';
+import catNames from 'cat-names';
+import { Slider } from '@miblanchard/react-native-slider';
+import GridFlatList from '../../src/index';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+  const fetchCats = () => {
+    fetch('https://api.thecatapi.com/v1/images/search?limit=5&page=1')
+      .then((res) => res.json())
+      .then((response) => {
+        let cats = response.map((item) => {
+          return {
+            id: item.id,
+            image: item.url,
+            name: catNames.random(),
+            rating: 0.5,
+          };
+        });
+        setData(cats);
+        // this.setLoading(false);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  useEffect(() => {
+    fetchCats();
   }, []);
+
+  const getMarkColor = (value: number) => {
+    return value > 0.9
+      ? '#4caf50'
+      : value > 0.8
+      ? '#8bc34a'
+      : value > 0.6
+      ? '#cddc39'
+      : value > 0.5
+      ? '#ffeb3b'
+      : value > 0.4
+      ? '#ffeb3b'
+      : value > 0.2
+      ? '#e91eee'
+      : '#e91e63';
+  };
+
+  const changeCatRating = (item, rating) => {
+    setData((prevData) => {
+      return prevData.map((prevItem) => {
+        return item.id === prevItem.id
+          ? {
+              ...item,
+              rating,
+            }
+          : prevItem;
+      });
+    });
+  };
+
+  function renderItem(item: any, index: number) {
+    return (
+      <View style={styles.card}>
+        <Image source={{ uri: item.image }} style={styles.cardImage} />
+        <View style={styles.cardContent}>
+          <Text style={styles.cardName}>{item.name}</Text>
+          <Slider
+            onValueChange={(rating) => changeCatRating(item, rating)}
+            value={item.rating}
+            minimumTrackTintColor={getMarkColor(item.rating)}
+          />
+          <View style={styles.cardMarkRow}>
+            <Text style={styles.cardMark}>Mark:</Text>
+            <Text style={{ color: getMarkColor(item.rating) }}>
+              {(item.rating * 10).toFixed(2)}
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <GridFlatList
+        data={data}
+        renderItem={renderItem}
+        paddingHorizontal={10}
+        paddingTop={10}
+      />
     </View>
   );
 }
@@ -20,12 +97,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+    backgroundColor: '#BCBCBC',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  card: {
+    alignItems: 'flex-start',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 6,
+    overflow: 'hidden',
+    flex: 1,
+    height: 240,
+    minHeight: 240,
+    width: '100%',
+    elevation: 5,
+  },
+  cardImage: {
+    height: 120,
+    width: '100%',
+  },
+  cardContent: { padding: 10, width: '100%' },
+  cardName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  cardMarkRow: {
+    flexDirection: 'row',
+  },
+  cardMark: {
+    fontWeight: 'bold',
+    marginRight: 10,
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,90 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import * as React from 'react';
+
+import { View, StyleSheet, FlatList } from 'react-native';
+
+interface GridFlatListInterface extends FlatList<any> {
+  data: any[];
+  renderItem: Function;
+  numColumns?: number;
+  gap?: number;
+  paddingHorizontal?: number;
+  paddingTop?: number;
 }
+
+export default function GridFlatList({
+  data,
+  renderItem,
+  numColumns = 2,
+  gap = 12,
+  paddingHorizontal = 0,
+  paddingTop = 0,
+  ...props
+}: GridFlatListInterface) {
+  const firstRowElementStyle = (index: number) => {
+    if (index < numColumns) {
+      return {
+        paddingTop,
+      };
+    }
+
+    return null;
+  };
+
+  const lastRowChildStyle = (index: number) => {
+    if ((index + 1) % numColumns === 0) {
+      return { paddingRight: paddingHorizontal, paddingLeft: 0 };
+    }
+    return null;
+  };
+
+  const lastOddChildStyle = (index: number) => {
+    if (index + 1 === data.length && (index + 1) % 2 !== 0) {
+      return {
+        paddingRight: gap + paddingHorizontal,
+      };
+    }
+
+    return null;
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={styles.container}
+        columnWrapperStyle={styles.row}
+        data={data}
+        numColumns={numColumns}
+        renderItem={({ item, index }) => (
+          <View
+            style={[
+              {
+                flex: 1 / numColumns,
+                paddingLeft: paddingHorizontal,
+                paddingBottom: gap,
+                paddingRight: gap,
+              },
+              firstRowElementStyle(index),
+              lastRowChildStyle(index),
+              lastOddChildStyle(index),
+            ]}
+          >
+            {renderItem(item, index)}
+          </View>
+        )}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(_item, index) => 'grid-list-item_' + index}
+        {...props}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    justifyContent: 'flex-start',
+  },
+  container: {
+    flex: 1,
+  },
+});
